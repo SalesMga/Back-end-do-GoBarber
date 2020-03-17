ebfcdce569765422e1b9665f5bb5c649f3400617
LucasSalesMariga <61717240+LucasSalesMariga@users.noreply.github.com>
2020-03-17 18:06:46 -0300

    Finalizado: Validando dados de entrada

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,17 +1,29 @@
 import jwt from 'jsonwebtoken';
 import authConfig from '../../config/auth';
 import User from '../models/User';
+import * as Yup from 'yup';
+
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+      .email()
+      .required(),
+      password: Yup.string().required(),
+    });
+
+    if (! (await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' })
+    };
     const { email, password } = req.body;
 
-    const user = await User.findOne({where : {email}});
+    const user = await User.findOne({where : {email:email}});
 
     if(!user){
       return res.status(401).json({error: 'User not found'});
     }
-    if(!(await user.checkPassword(password))){
+    if(! (await user.checkPassword(password))){
     return res.status(401).json({error: 'Password does not match'});
   }
 
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -19,13 +19,15 @@ class User extends Model {
     );
     this.addHook('beforeSave', async (user) => {
       if (user.password) {
-        user.password_hash = await bcrypt.hash(user.password, 10);
+        user.password_hash = await bcrypt.hash(user.password, 8);
       }
     });
     return this;
   }
   checkPassword(password) {
-    return bcrypt.compare(password, this.password_hash);
+    console.log(password);
+    return (bcrypt.compare(password, this.password_hash));
+
   }
 }
 
