f141d4af3cfe39bbc56a2cc1376da171c8c03e69
LucasSalesMariga <61717240+LucasSalesMariga@users.noreply.github.com>
2020-04-02 23:02:46 -0300

    Listando agendamentos de servidor

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,36 @@
+import Appointments from '../models/Appointment';
+import User from '../models/User';
+import {startOfDay, endOfDay, parseISO} from 'date-fns'
+import {Op} from 'sequelize';
+
+class ScheduleController {
+  async index(req, res){
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId , provider: true},
+    });
+
+    if(!checkUserProvider){
+      return res.status(401).json({error: 'User is not a provider'});
+    }
+    const {date} = req.query;
+    const parsedDate = parseISO(date);
+
+    // incio "2020-04-03 00:00:00 todos agendamento entre o a data do dia.
+    //fim "2020-04-03 23:59:59
+
+    const appointments = await Appointments.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date:{
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date'],
+    });
+    return res.json(appointments);
+}
+}
+
+export default new ScheduleController();
+
