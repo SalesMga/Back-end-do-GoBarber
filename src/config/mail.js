3b72b077c9f4390639aaf329117590e9780a2e16
LucasSalesMariga <61717240+LucasSalesMariga@users.noreply.github.com>
2020-04-07 21:14:56 -0300

    Configurando NodeMailer

---
+++ src/config/mail.js
@@ -0,0 +1,13 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "14b65f1806fed9",
+    pass: "9a12e34b463b70"
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>'
+  },
+};
+
